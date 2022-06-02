cedcb20444469683b89f914a6f51fe1194bcf17d
mikel <mikeltxomikeltxo@gmail.com>
2022-06-02 15:32:37 +0200

    dia 2

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-function recogerDatos() {
-    
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const telefono = document.getElementById('telefono').value;
+const boton = document.getElementById('button');
+const contactos = document.querySelector("contactos");
+const ul = document.getElementById("datos_ul");
+const div = document.getElementById("div");
 
-    if (nombre == '' || apellido == '' || telefono == '') {
-        alert("Contacto no valido, Faltan campos!!");
-        return; 
+boton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let nombre = document.getElementById('nombre');
+    let apellido = document.getElementById('apellido');
+    let telefono = document.getElementById('telefono');
+    if (nombre.value == '' || apellido.value == '' || telefono.value == '') {
+        alert("Campos vacios o incorrectos!");
+        return;
     }
+    const datos = [nombre.value, apellido.value, telefono.value];
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = "    "+nombre.value+"    "+apellido.value+"  :    "+telefono.value;
+    li.appendChild(añadirBoton());
+    li.appendChild(p);
+    ul.appendChild(li);
+    div.appendChild(ul);
+    
+    nombre.value= "";
+    apellido.value= "";
+    telefono.value= "";
+});
 
-    const div = document.createElement('div');
-    const p1 = document.createElement('p');
-    const p2 = document.createElement('p');
-    const p3 = document.createElement('p');
-    p1.textContent = nombre;
-    p2.textContent = apellido;
-    p3.textContent = telefono;
-    div.appendChild(p1, p2, p3);
-
-    const currentDiv = document.getElementById('contactos');
-    document.body.insertBefore(div, currentDiv);
+function añadirBoton() {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "btn_delete";
+    deleteBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+    })
+    return deleteBtn;
 }
-
-
